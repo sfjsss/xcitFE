9c95b6bd032eee8a4fdbebb94c815a66d8753459
Tianyu Li <alandron06281990@gmail.com>
2020-08-05 22:32:24 -0700

    done, next work on floating buttons

--- src/app/main/home/home.page.ts
+++ src/app/main/home/home.page.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from "rxjs";
+import {MainService} from "../main.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
+    selector: 'app-home',
+    templateUrl: './home.page.html',
+    styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage implements OnInit, OnDestroy {
+    private showAddMenu : boolean;
+    private showAddMenuSub : Subscription;
 
-  constructor() { }
+    constructor(private mainService : MainService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.showAddMenuSub = this.mainService.showAddMenu.subscribe(currState => {
+            this.showAddMenu = currState;
+        })
+    }
+
+    onClickBackdrop() {
+        console.log("backdrop is clicked");
+        this.mainService.toggleAddMenu();
+    }
+
+    ngOnDestroy(): void {
+        if (this.showAddMenuSub) {
+            this.showAddMenuSub.unsubscribe();
+        }
+    }
 
 }
